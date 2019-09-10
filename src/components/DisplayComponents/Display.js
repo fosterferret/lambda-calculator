131cb18b4d23de5c21743086e3ee36f9f6f0a21c
Francis Bulus <notchera@gmail.com>
2019-09-10 13:02:29 +0100

    rendered total value within display and exported the display component

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+const Display = ({ totalValue }) => {
+  return (
+    <div className="display">
+      {" "}
+      <div className="displayDigit">{totalValue}</div>
+    </div>
+  );
 };
+
+export default Display;
